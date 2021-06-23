51ddc1d6a7f41357c98587d2b1c8380b1fdab23d
Anna Tsikhachova <ganna13666@gmail.com>
2021-06-23 16:50:54 +0300

    changes in hw

--- hw.js
+++ hw.js
@@ -1,3 +1,6 @@
-let age = prompt('Сколько тебе лет?')
+const age = prompt('Сколько тебе лет?')
 alert(age)
 console.log(age)
+let message = 'Несмотря на преклонные годы, ты все еще молод душой'
+alert (message)
+console.log(message)
